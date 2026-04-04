e12d3b012e37598b2dc5c75df140e220a55f4cf0
Gregory Cook <cook.oniel@gmail.com>
2026-04-04 13:25:24 -0400

    k8manifests

--- src/HealthPulse.Api/Program.cs
+++ src/HealthPulse.Api/Program.cs
@@ -42,13 +42,6 @@ builder.Services.AddOpenTelemetry()
 
 var app = builder.Build();
 
-// Apply pending migrations on startup
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<HealthPulseDbContext>();
-    db.Database.Migrate();
-}
-
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
